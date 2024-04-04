7c41588865413da6c62342241ce4f4b3d4611be1
Till Vogelsang <tillvogelsang@icloud.com>
2024-04-04 14:33:06 +0200

    feat: added message converter

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,9 @@
 package de.till
-
 fun main() {
     println("Hello World!")
+    val message = "Some *Markdown* <@123456>"
+
+    val htmlMessage = MessageConverter.convert(message)
+    val htmlMessageMentions = MessageConverter.replaceEscapedMentions(htmlMessage)
+    println(htmlMessageMentions)
 }
