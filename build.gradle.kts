9f7fa9f7c4edadc276b10c1fb9fe8702f65e6c64
Till Vogelsang <tillvogelsang@icloud.com>
2024-04-04 14:32:42 +0200

    build: added dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,12 +5,23 @@ plugins {
 group = "de.till"
 version = "1.0-SNAPSHOT"
 
+val jdaVersion: String by project
+val junitVersion: String by project
+
+
 repositories {
     mavenCentral()
+    maven("https://nexus.flawcra.cc/repository/mirrors/")
+    maven("https://repo.fruxz.dev/releases/")
+
 }
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+    implementation("org.jetbrains:markdown:0.6.1")
+    implementation("dev.fruxz:ascend:2024.1.1")
+    implementation("net.dv8tion:JDA:$jdaVersion")
+    implementation("org.junit.jupiter:junit-jupiter:$junitVersion")
 }
 
 tasks.test {
