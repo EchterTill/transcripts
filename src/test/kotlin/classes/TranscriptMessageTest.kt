c6f52283f0083438e2505fc9b9f5b907cb20baeb
Till Vogelsang <tillvogelsang@icloud.com>
2024-04-05 14:37:37 +0200

    feat: added unit tests

---
+++ src/test/kotlin/classes/TranscriptMessageTest.kt
@@ -0,0 +1,17 @@
+package classes
+
+import de.till.classes.TranscriptMessage
+import dev.fruxz.ascend.tool.time.calendar.Calendar
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class TranscriptMessageTest {
+    @Test
+    fun testToHTML() {
+        val expected = "<p><em><strong>Hello</strong></em> <span class=\"user-mention\">world</span></p>"
+        val message = TranscriptMessage(
+            "***Hello*** <@1234>", "echtertill", "1234", Calendar.now(), mapOf(Pair<String, String>("1234", "world"))
+        )
+        Assertions.assertEquals(expected, message.toHTML())
+    }
+}
