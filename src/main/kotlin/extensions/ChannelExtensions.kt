15bf4d8ec08fd821cc3aa87233e5c860b5f122a3
Till Vogelsang <tillvogelsang@icloud.com>
2024-04-05 19:47:13 +0200

    feat: added pdf send extension

--- src/main/kotlin/extensions/ChannelExtensions.kt
+++ src/main/kotlin/extensions/ChannelExtensions.kt
@@ -2,6 +2,7 @@ package de.till.transcripts.extensions
 
 import de.till.transcripts.classes.Transcript
 import de.till.transcripts.classes.TranscriptMessage
+import de.till.transcripts.pdf.pdfFromHTML
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel
 import net.dv8tion.jda.api.requests.restaction.MessageCreateAction
 import net.dv8tion.jda.api.utils.FileUpload
@@ -18,4 +19,14 @@ fun MessageChannel.sendTranscript(limit: Int): MessageCreateAction {
     val file = FileUpload.fromData(transcript.toHTML().toByteArray(), "transcript.html")
 
     return this.sendFiles(file)
+}
+
+fun MessageChannel.sendTranscriptAsPDF(limit: Int): MessageCreateAction {
+    val transcript = this.getTranscript(limit)
+
+    val pdf = pdfFromHTML(transcript.toHTML())
+
+    return this.sendFiles(FileUpload.fromData(pdf, "transcript.pdf"))
+
+
 }
