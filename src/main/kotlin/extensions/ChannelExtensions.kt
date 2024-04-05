e7b87a42d5ad4e21e8f329c923e7796622b760c0
Till Vogelsang <tillvogelsang@icloud.com>
2024-04-05 18:42:21 +0200

    feat: added extension function to send transcript to channels

---
+++ src/main/kotlin/extensions/ChannelExtensions.kt
@@ -0,0 +1,21 @@
+package de.till.transcripts.extensions
+
+import de.till.transcripts.classes.Transcript
+import de.till.transcripts.classes.TranscriptMessage
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel
+import net.dv8tion.jda.api.requests.restaction.MessageCreateAction
+import net.dv8tion.jda.api.utils.FileUpload
+
+fun MessageChannel.getTranscript(limit: Int): Transcript {
+    val history = this.getHistoryFromBeginning(limit).complete()
+
+    return Transcript(history.retrievedHistory.map { TranscriptMessage.from(it) }.reversed())
+}
+
+fun MessageChannel.sendTranscript(limit: Int): MessageCreateAction {
+    val transcript = this.getTranscript(limit)
+
+    val file = FileUpload.fromData(transcript.toHTML().toByteArray(), "transcript.html")
+
+    return this.sendFiles(file)
+}
