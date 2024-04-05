3a2339feaadd3205b9552b7b68d00a247231a05b
Till Vogelsang <tillvogelsang@icloud.com>
2024-04-05 17:58:37 +0200

    feat: updated unit test

--- src/test/kotlin/classes/TranscriptMessageTest.kt
+++ src/test/kotlin/classes/TranscriptMessageTest.kt
@@ -8,9 +8,13 @@ import org.junit.jupiter.api.Test
 class TranscriptMessageTest {
     @Test
     fun testToHTML() {
-        val expected = "<p><em><strong>Hello</strong></em> <span class=\"user-mention\">world</span></p>"
+        val expected = "<p time=\"17:57\"><em><strong>Hello</strong></em> <span class=\"user-mention\">world</span></p>"
         val message = TranscriptMessage(
-            "***Hello*** <@1234>", "echtertill", "1234", Calendar.now(), mapOf(Pair<String, String>("1234", "world"))
+            "***Hello*** <@1234>",
+            "echtertill",
+            "1234",
+            Calendar.fromMilliseconds(1712332647000),
+            mapOf(Pair<String, String>("1234", "world"))
         )
         Assertions.assertEquals(expected, message.toHTML())
     }
